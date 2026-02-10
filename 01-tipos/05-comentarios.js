cc44cbd413a53ce20c95ab2436441588a1535865
Juan Esteban David <davidcanojuanesteban92@gmail.com>
2026-02-10 14:31:52 -0500

    se subio nuevo archico

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,13 @@
+
+//
+/**
+ * Comentarios en JavaScript
+ * 
+ * Los comentarios son líneas de texto que el intérprete de JavaScript ignora. Se utilizan para explicar el código, hacer anotaciones o desactivar temporalmente partes del código.
+ */
+
+/**let numero = 42; // Este es un número
+let nombre = "Juan";
+
+console.log(numero); // Imprime el número
+console.log(nombre); // Imprime el nombre */
