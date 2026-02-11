e1186233178c249b8ae4e9f1165d2b58f54f561b
Juan Esteban David <davidcanojuanesteban92@gmail.com>
2026-02-11 17:19:32 -0500

    actualizando reporsitorio mas fundamentos de js

--- 01-tipos/09-funciones1.1.js
+++ 01-tipos/09-funciones1.1.js
@@ -1,4 +1,39 @@
-function suma(){
-    return 2 + 2;
+function suma(a){  // a es un parametro de la funcion
+    return a + 2;
+}
+
+let resultado = suma(3); // arguemntos (3) ) y el valor de retorno es 5
+
+console.log(resultado);
+
+//-------------------------------------------
+
+function nombre(a){
+    return a + " Esteban";
+}
+    
+let nombreCompleto = nombre("luciano"); 
+console.log(nombreCompleto); 
+
+//------------------------------------------
+
+function archivo(a){
+    return  a + " formato";
 
 }
+
+let formatoArchivo = archivo("pdf");
+console.log(formatoArchivo);  
+
+
+
+//-------------------------
+
+function suma(a,b){ 
+    console.log(arguments);
+    return a + b;
+}
+
+let resultado1 = suma(3,7,10,15,7,9,); // arguemntos (3,7,10,15) ) y el valor de retorno es 10
+console.log(resultado1);
+console.log(typeof resultado1);
