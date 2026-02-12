2d985400ea0b7cce42006112799bad7670bb9a30
Juan Esteban David <davidcanojuanesteban92@gmail.com>
2026-02-12 18:11:51 -0500

    que agrego nuevo tema operadores

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,35 @@
+    let a = 10  // DECLARACION
+    let b = 10
+
+// 
+    console.log(a + b , "SUMA") // SUMA
+    console.log(a - b, "RESTA") // RESTA
+    console.log(a * b, "MULTIPLICACION") // MULTIPLICACION
+    console.log(a / b, "DIVISION") // DIVISION
+    console.log(a % b, "MODULO O RESIDUO") // MODULO O RESIDUO
+    console.log(a ** b, "POTENCIA") // POTENCIA
+
+    // INCREMENTO
+
+     
+    console.log(a++);
+    console.log(++a);
+     
+    console.log(a) // 11
+
+    // DECREMENTO
+
+    console.log(b--) // POST DECREMENTO
+    console.log(--b) // PRE DECREMENTO
+
+    console.log(b) // 4
+
+
+    
+
+
+
+
+
+
+
