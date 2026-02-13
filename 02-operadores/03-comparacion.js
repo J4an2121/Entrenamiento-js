3279e4b072bbe2e20d4b38966ef553f332a6cfd4
Juan Esteban David <davidcanojuanesteban92@gmail.com>
2026-02-13 18:05:07 -0500

    se finalizo el tema de operadores

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+
+// OPERADORES DE COMPARACION
+
+console.lot(a > 5); // mayor que
+console.log(a < 5); // menor que
+console.log(a >= 5); // mayor o igual que
+console.log(a <= 5); // menor o igual que
+
+console.log(a == 5); // igual que
+console.log(a != 5); // diferente que
+console.log(a === 5); // estrictamente igual que
+console.log(a !== 5); // estrictamente diferente que
---
+++ 02-operadores/08-ternario.js
@@ -0,0 +1,5 @@
+// los operedaores ternarios son una forma concisa de escribir una expresión condicional en JavaScript. Se utilizan para asignar un valor a una variable en función de una condición, y su sintaxis es la siguiente:
+
+let edad = 18;
+let acceso = edad >= 18 ? "Acceso permitido" : "Acceso denegado";
+console.log(acceso); // "Acceso permitido"  
