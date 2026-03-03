6c806a7fdee8730b61ee2bcdb8715ac8fb1985dc
Juan Esteban David <davidcanojuanesteban92@gmail.com>
2026-03-03 18:13:30 -0500

    COMIENDO EL TEMA POO

--- 03-control_de_flujo/10-switch.js
+++ 03-control_de_flujo/10-switch.js
@@ -1,3 +1,15 @@
 // switch es una estructura de control de flujo que permite ejecutar diferentes bloques de código en función del valor de una expresión. Es similar a una serie de sentencias if-else, pero puede ser más legible y eficiente en algunos casos.
 
 // sintaxis: 
+
+ let accion = "listar";
+
+ switch (accion) {
+    case "listar":
+        console.log("ACCIÓN: LISTAR");
+        break; // el break es necesario para evitar que se ejecuten los siguientes casos    
+    case "guardar":
+        console.log("ACCIÓN: GUARDAR");
+
+ }
+
